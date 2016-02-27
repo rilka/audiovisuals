9bb7f4230ad229852af0e3e49f074c296bb34863
Angela Li <angelali@dropbox.com>
2016-02-27 13:07:40 -0500

    Test loading static files augh

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,12 @@
 "use strict";
 
 const AUDIO_SOURCES = [
-    '/mp3/Massive Attack - Teardrop.mp3',
-    '/mp3/Mount Kimbie - Carbonated.mp3',
-    '/mp3/Capital Cities - Safe and Sound (DiscoTech Remix).mp3',
-    '/mp3/Kygo - Here For You (feat. Ella Henderson).mp3',
-    '/mp3/The Cinematic Orchestra - Arrival Of The Birds.mp3',
-    '/mp3/What So Not - High You Are (Branchez Remix).mp3',
-
+    'mp3/Massive Attack - Teardrop.mp3',
+    'mp3/Mount Kimbie - Carbonated.mp3',
+    'mp3/Capital Cities - Safe and Sound (DiscoTech Remix).mp3',
+    'mp3/Kygo - Here For You (feat. Ella Henderson).mp3',
+    'mp3/The Cinematic Orchestra - Arrival Of The Birds.mp3',
+    'mp3/What So Not - High You Are (Branchez Remix).mp3',
 ];
 
 var audioFile, analyzer;
